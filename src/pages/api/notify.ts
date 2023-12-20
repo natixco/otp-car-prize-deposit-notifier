15ff81062b334a039361786ab659dabe0dc47ae6
natixco <patrikvisloczki@gmail.com>
2023-12-20 14:28:44 +0100

    fix: cron jobs

--- src/pages/api/notify.ts
+++ src/pages/api/notify.ts
@@ -3,8 +3,8 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { Resend } from "resend";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'GET') {
-    return res.status(405).json({ error: 'Method not allowed' });
+  if (req.headers && req.headers.Authorization !== `Bearer ${process.env.CRON_SECRET}`) {
+    return res.status(401).end('Unauthorized');
   }
 
   const prisma = new PrismaClient();
--- src/pages/api/update.ts
+++ src/pages/api/update.ts
@@ -8,8 +8,8 @@ const selectors = {
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'GET') {
-    return res.status(405).json({ error: 'Method not allowed' });
+  if (req.headers && req.headers.Authorization !== `Bearer ${process.env.CRON_SECRET}`) {
+    return res.status(401).end('Unauthorized');
   }
 
   const prisma = new PrismaClient();
