f311680465a026cf250997200ae981e100badef2
natixco <patrikvisloczki@gmail.com>
2023-12-20 17:13:26 +0100

    fix?: schema

--- drizzle/0003_glorious_vivisector.sql
+++ drizzle/0003_glorious_vivisector.sql
@@ -1,5 +1,5 @@
 DROP INDEX IF EXISTS "userId_idx";--> statement-breakpoint
-ALTER TABLE "account" ADD CONSTRAINT "account_provider_providerAccountId_pk" PRIMARY KEY("provider","providerAccountId");--> statement-breakpoint
-ALTER TABLE "verificationToken" ADD CONSTRAINT "verificationToken_identifier_token_pk" PRIMARY KEY("identifier","token");--> statement-breakpoint
 ALTER TABLE "account" DROP COLUMN IF EXISTS "id";--> statement-breakpoint
 ALTER TABLE "verificationToken" DROP COLUMN IF EXISTS "id";
+ALTER TABLE "account" ADD CONSTRAINT "account_provider_providerAccountId_pk" PRIMARY KEY("provider","providerAccountId");--> statement-breakpoint
+ALTER TABLE "verificationToken" ADD CONSTRAINT "verificationToken_identifier_token_pk" PRIMARY KEY("identifier","token");--> statement-breakpoint
