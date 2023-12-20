667944404ed202f0ae60ac9b711a7f5b8618f4fa
natixco <patrikvisloczki@gmail.com>
2023-12-20 18:30:20 +0100

    refactor: changed footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="py-6 bg-zinc-900 w-full">
+    <footer className="py-8 bg-zinc-900 w-full">
       <div className="main-container flex flex-row items-center justify-center">
         <p className="font-semibold text-zinc-200 text-center leading-6">
-          Az oldal semmilyen jogi kapcsolatban nem áll az OTP bankkal.
-          <br/>
-          Csak a nyertes betétekről küld emailt az oldal.
-          <br/>
+          Az oldal nem áll semmilyen kapcsolatban az OTP bankkal.
           A teljes forráskód <a href="https://github.com/natixco/otp-car-prize-deposit-notifier" className="underline hover:text-lime-500">GitHub-on</a> megtalálható.
         </p>
       </div>
